56c9b9d94ce21c9c08e74f44815e604099385f90
Divya Salunkhe <salunkhedivya67@gmail.com>
2024-10-04 10:24:01 +0530

    Add files via upload

---
+++ pr 4/q.7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i , j, k;
+	
+	for(i=5;i<=5;i++)
+	{
+		for(j=i;j<=1;j++)
+		{
+			if(i=1 || j==1 || i==3 || i==2 && j==5)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		
+	}
+}
--- q.1.c
+++ q.1.c
@@ -1,31 +1,15 @@
 #include<stdio.h>
-#include<conio.h>
+
 
 void main()
-{ 
-     int num;
-     
-     printf("enter the 1-7 number:");
-     scanf("%d",&num);
-     
-     switch(num)
-     
-     {
-     	case1:printf("monday");
-     	break;
-     	case 2:printf("tuesday");
-     	break;
-		case 3:printf("wenday");
-     	break;
-     	case 4:printf("trusday");
-     	break;
-		case 5:printf("friday");
-     	break;
-     	case 6:printf("saturday");
-     	break;
-     	case 7:printf("sunday");
-     	break;
-	 }
-
-	 
+{
+	int i,j;
+	for(i=41; i<=45; i++)
+	{
+		for(j=41; j<=i; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
 }
