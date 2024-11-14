7240e7b3812ac485c7f244fabf46b09d1f2eb7a3
Divya Salunkhe <salunkhedivya67@gmail.com>
2024-11-14 13:17:08 +0530

    Add files via upload

---
+++ array/q1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	printf("Enter a size of array :");
+	scanf("%d",&n);
+	
+	int a[n];
+	
+	int i;
+	for(i=0; i<n; i++)
+	{
+		printf("Enter a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+	
+	    printf("/n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("%d",a[i]);
+		
+		
+	}
+	
+}
+      
